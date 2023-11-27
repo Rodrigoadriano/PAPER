1550cfbf5433b99351fa534baf6760fcf56ac862
Rodrigo Adriano <rdgadriano@gmail.com>
2023-11-27 15:01:20 -0300

    Adicionado outro metodo para fazer o json

--- lista2.php
+++ lista2.php
@@ -12,6 +12,25 @@ $sql = "SELECT * FROM funcionario";
 $result = $conn->query($sql);
 
 // Se houver resultados
+////////////////////////////////////////////////////////
+// Obtém os nomes das colunas
+//$columns = array();
+//while ($finfo = $result->fetch_field()) {
+//    $columns[] = $finfo->name;
+//}
+
+// Obtém os dados das linhas
+//while ($row = $result->fetch_assoc()) {
+  //  $rowData = array();
+  //  foreach ($columns as $col) {
+ //       $rowData[$col] = $row[$col];
+//    }
+//    $rows[] = $rowData; // Adiciona cada linha ao array $rows
+//}
+//
+// Converte o array $rows para JSON usando json_encode()
+//$json = json_encode($rows);
+///////////////////////////////////////////////////////////
 if ($result->num_rows > 0) {
 
     $rows = array(); // Array para armazenar os resultados
@@ -24,8 +43,9 @@ if ($result->num_rows > 0) {
     $json = json_encode($rows);
 
     // Retorna ou imprime o JSON
+    header('Content-Type: application/json');
     echo $json;
-
+    
 
   
 } else {
