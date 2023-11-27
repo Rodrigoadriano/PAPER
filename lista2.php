d9244c9d27f78d02e4bb5a761bc3cdbf958f8d14
Rodrigo Adriano <rdgadriano@gmail.com>
2023-11-27 14:36:08 -0300

    Update lista2.php

--- lista2.php
+++ lista2.php
@@ -1,1 +1,28 @@
-Lista 2
+<?php
+// Conectar ao banco de dados (substitua essas informações com suas credenciais)
+include "conexao.php";
+$conn = $conexao;
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+// Query para selecionar todos os funcionários
+$sql = "SELECT * FROM funcionario";
+$result = $conn->query($sql);
+
+// Se houver resultados
+if ($result->num_rows > 0) {
+
+
+  
+} else {
+  
+ echo "0 resultados";
+  
+}
+
+
+// Feche a conexão
+$conn->close();
+?>
